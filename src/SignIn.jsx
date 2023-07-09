7ce3bca4a8b8331bd69eb1affd4de1eb5e146cea
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-07-09 16:07:33 +0100

    log in

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -2,6 +2,7 @@ export default function SignIn(){
     return(
         <div className="loginPage">
             <form className="loginForm">
+                <div>Log in to LB</div>
                 <input type="email" placeholder="Email address" />
                 <input type="password" placeholder="Password" />
                 <input type="button" value="Log in" />
